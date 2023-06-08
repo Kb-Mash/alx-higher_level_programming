4210f38e75de11134ddbf4df94600691c3894325
Kb-Mash <karabo.kmashile@gmail.com>
2023-06-08 13:05:22 -0700

    advanced task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    ops = {'+': add, '-': sub, '*': mul, '/': div}
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+    if op in ops:
+        result = ops[op](a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, result))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
