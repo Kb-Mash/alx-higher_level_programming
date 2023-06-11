ace7d65a05ece8753ef6446b2e12e5f902c0fbc2
Kb-Mash <karabo.kmashile@gmail.com>
2023-06-11 12:30:41 -0700

    incorrect output: fixed error: used == instead of = for assignment

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,6 +5,6 @@ def max_integer(my_list=[]):
         return (None)
     big = my_list[0]
     for i in my_list:
-        if big < i:
-            big == i
+        if i > big:
+            big = i
     return (big)
