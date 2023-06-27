28e00d8993c1bf0bf5a750b7b353189f482a7f32
Kb-Mash <karabo.kmashile@gmail.com>
2023-06-27 10:26:07 -0700

    incorrect output update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,24 +4,25 @@
 
 class Square:
     """defines a square"""
+
     def __init__(self, size=0):
-        """Instantiation with optional"""
-        self.__size = size
+        """Instantiation with optional size"""
+        self.size = size
 
-        @property
-        def size(self):
-            """retrieves size of square"""
-            return self.__size
+    @property
+    def size(self):
+        """to retrieve current size of the square."""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            """set size"""
-            if not isinstance(value, int):
-                raise TypeError('size must be an integer')
-            elif value < 0:
-                raise ValueError('size must be >= 0')
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """to set size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """return the current square area"""
-            return (self.__size ** 2)
+    def area(self):
+        """return the current square area"""
+        return (self.__size * self.__size)
