1d66f8165d4721df40514f279a15374daf32db8b
Kb-Mash <karabo.kmashile@gmail.com>
2023-07-05 05:01:29 -0700

    task 0 and 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a function that divides all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """Returns a new matrix of results"""
+
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
+
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
