2b70d60da032b1698bf56a401937beabb8bad141
Kb-Mash <karabo.kmashile@gmail.com>
2023-07-10 11:34:03 -0700

    incorrect output update

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,5 +10,5 @@ class Square(Rectangle):
     def __init__(self, size):
         """initialize a new square"""
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
