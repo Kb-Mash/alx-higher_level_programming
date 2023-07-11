62c11e3bf8a30ea1973a7cd2b4e25d5c96ea3a87
Kb-Mash <karabo.kmashile@gmail.com>
2023-07-11 11:43:47 -0700

    append after update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,11 +4,10 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """Insert text after each line containing a given string in a file"""
-    with open(filename, "r") as fp:
-        lines = fp.readlines()
-
-    with open(filename, "w") as fp:
-        for line in lines:
-            fp.write(line)
+    with open(filename) as fp:
+        for line in fp:
+            text += line
             if search_string in line:
-                fp.write(new_string + "\n")
+                text += new_string
+    with open(filename, "w") as fp:
+        fp.write(text)
