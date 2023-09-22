1f791319daad77c169335dcebb1c581123ffc0c2
Kb-Mash <karabo.kmashile@gmail.com>
2023-09-22 08:20:35 -0700

    relationship fix

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
-"""Module that contains the class definition of a City"""
+""class definition of a City"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from relationship_state import Base
 from sys import argv
-
-Base = declarative_base()
 
 
 class City(Base):
@@ -19,6 +17,7 @@ class City(Base):
         state_id (sqlalchemy.Column): The city's state id.
     """
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", foreign_keys=state_id)
